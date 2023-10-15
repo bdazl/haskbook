cc475fddb93b6d72c9119e9b3f42fe43d757be6d
Jacob Peyron <jacob.peyron@gmail.com>
2023-10-15 16:39:16 +0200

    added optparse-applicative (much simpler than cabal that takes all day)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
-
+import Control.Monad
+import Options.Applicative
 import Chapter5
 
+-- data Options = Options{ optCommand :: Command }
+-- data Command = Chap5 | Default
+
+opts :: Parser (IO ())
+opts = subparser
+    ( command "5" (info (pure chapter5) idm)
+   <> command "dummy" (info (pure dummy) idm)
+    )
+
+dummy :: IO ()
+dummy = putStrLn "I'm dummy"
+
 main :: IO ()
-main = chapter5
+main = join $ execParser (info opts idm)
